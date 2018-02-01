c1aecfb1b9fc370241792812f2bf21dd204b2815
r-i-c-h <rich@richwerden.com>
2018-01-31 19:36:43 -0800

    Cosmetic fixes and Var names

--- actions/logic.js
+++ actions/logic.js
@@ -2,10 +2,10 @@ const getRightToLeftMaxes = (arr) => {
   let max = 0;
   return arr.reduceRight( (retArr, elem) => {
     if (elem > max) { max = elem; }
-    retArr = [max, ...retArr]
+    retArr = [max, ...retArr];
     return retArr;
   },[]);
-}
+};
 
 const getPuddles = (arr) => {
   let max = 0;
@@ -16,17 +16,17 @@ const getPuddles = (arr) => {
     const waterAmt = slotMax - elem;
     return waterAmt < 0 ? 0 : waterAmt;
   });
-}
+};
 
 const getBiggestPuddle = (arr) => {
-  const invertGraph = getPuddles( arr.slice() ); 
+  const invertGraph = getPuddles( arr.slice() );
   let ansArr = [null,null,0];
 
   for (let i = 0; i < invertGraph.length - 1; i++) {  // From first to 2nd-to-last
     if (invertGraph[i] === 0 && invertGraph[i+1] !== 0) {
       let subArr = [];
       let runningSum = 0;
-      
+
       subArr.push(i);
       while (invertGraph[i+1] !== 0) {
         runningSum += invertGraph[i+1];
@@ -39,6 +39,6 @@ const getBiggestPuddle = (arr) => {
     }
   }
   return ansArr;
-}
+};
 
-module.exports = { getRightToLeftMaxes, getPuddles, getBiggestPuddle }
+module.exports = { getRightToLeftMaxes, getPuddles, getBiggestPuddle };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const PORT = 1337;
-const app = require('./app')
+const app = require('./app');
 
 
 app.listen(PORT, () => console.log(`Server up and listening on port ${PORT}`) );
